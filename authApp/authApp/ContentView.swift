44a624d502a3323432c5baf03a83f184271a479f
michael <code21032@gmail.com>
2019-10-07 23:38:02 +0900

    *update faceid sample

---
+++ authApp/authApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  authApp
+//
+//  Created by Michael LIM on 07/10/2019.
+//  Copyright © 2019 topco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          Text("Target Color Block")
+          Text("Guess Color Block")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
